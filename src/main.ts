3ec23dfbdc9613d276af39bf6bec87d57f841fc9
Felipe <felipematheus17@hotmail.com>
2026-01-15 01:05:42 -0300

    feat: Initialize NestJS application with CORS, Kafka microservices, and Swagger documentation.

--- src/main.ts
+++ src/main.ts
@@ -8,7 +8,7 @@ async function bootstrap() {
   app.setGlobalPrefix('api');
 
   app.enableCors({
-    origin: ['http://localhost:3000', 'http://192.168.0.5:3000'],
+    origin: [process.env.FRONTEND_URL || 'http://localhost:3000'],
     credentials: true,
     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
   });
